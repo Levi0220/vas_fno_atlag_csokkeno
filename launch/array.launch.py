98dfea7324402266b2ea114478af937d71296e76
Levi0220 <levente.vass@ddc.sze.hu>
2025-10-05 15:27:18 +0200

    regi file nev a launch fileban

--- launch/array.launch.py
+++ launch/array.launch.py
@@ -3,7 +3,7 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        Node(package='vas_fno_kisprojekt', executable='array_publisher', name='array_publisher', output='screen'),
-        Node(package='vas_fno_kisprojekt', executable='array_sorter',    name='array_sorter',    output='screen'),
-        Node(package='vas_fno_kisprojekt', executable='array_averager',  name='array_averager',  output='screen'),
+        Node(package='vas_fno_atlag_csokkeno', executable='array_publisher', name='array_publisher', output='screen'),
+        Node(package='vas_fno_atlag_csokkeno', executable='array_sorter',    name='array_sorter',    output='screen'),
+        Node(package='vas_fno_atlag_csokkeno', executable='array_averager',  name='array_averager',  output='screen'),
     ])
